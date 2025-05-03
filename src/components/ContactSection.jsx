d9d729e0dbd84d3b8b400f6f90d16a803ea1418e
ClaraaaL <babananacream@gmail.com>
2025-05-02 21:44:12 -0600

    change email

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -75,7 +75,7 @@ export const ContactSection = () => {
                                 </div>
                                 <div>
                                     <h4 className="font-medium">Email</h4>
-                                    <a href="mailto:imdoralee@gmail.com" className="text-muted-foreground hover:text-primary transition-colors">
+                                    <a href="mailto:fortestuse25@gmail.com" className="text-muted-foreground hover:text-primary transition-colors">
                                         Click here to send an email to me
                                     </a>
                                 </div>
